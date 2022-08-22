cc7d717918300fd8226294436c6664e049af6734
Ohkubo KOHEI <ohkubo@heartrails.com>
2022-08-22 09:13:57 +0000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 set -ue
 
-
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends fish
-chsh -s /usr/bin/fish vscode
+sudo apt-get update
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y install --no-install-recommends fish
+sudo chsh -s /usr/bin/fish $USER
 
 # http://asdf-vm.com/guide/getting-started.html#_3-install-asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
-mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+mkdir -p ~/.config/fish/completions; ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
 echo "source ~/.asdf/asdf.fish" >> $HOME/.config/fish/config.fish
 
-# RUN asdf plugin add deno && asdf install deno latest && asdf global deno latest
-# RUN asdf plugin add golang && asdf install golang latest && asdf global golang latest
-# RUN asdf plugin add rust && asdf install rust latest && asdf global rust latest
-
+source ~/.asdf/asdf.sh
 asdf plugin add nodejs
 asdf plugin add rust
+# asdf install rust latest && asdf global rust latest
 asdf plugin add deno
+# asdf install deno latest && asdf global deno latest
