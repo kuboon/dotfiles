eb5e33cd94856263c213e93d0e8763a22b44b1d3
Ohkubo KOHEI <ohkubo@heartrails.com>
2022-09-07 18:02:13 +0900

    add lazygit

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -ue
 
-sudo apt-get update
-DEBIAN_FRONTEND=noninteractive sudo apt-get -y install --no-install-recommends fish
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update
+DEBIAN_FRONTEND=noninteractive sudo apt-get -y install --no-install-recommends fish gh
 sudo chsh -s /usr/bin/fish $USER
 
 # http://asdf-vm.com/guide/getting-started.html#_3-install-asdf
@@ -12,7 +15,11 @@ echo "source ~/.asdf/asdf.fish" >> $HOME/.config/fish/config.fish
 
 source ~/.asdf/asdf.sh
 asdf plugin add nodejs
+asdf plugin add golang
+asdf install golang latest; asdf global golang latest
 asdf plugin add rust
 # asdf install rust latest && asdf global rust latest
 asdf plugin add deno
 # asdf install deno latest && asdf global deno latest
+
+go install github.com/jesseduffield/lazygit@latest
