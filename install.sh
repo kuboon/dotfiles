3fadafa293269df533a039f18860728f3d96d569
Ohkubo KOHEI <kuboon@trick-with.net>
2024-01-11 11:55:01 +0900

    asdf -> mise

--- install.sh
+++ install.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/env bash
 set -ue
 
+sudo apt update
+sudo apt install -y software-properties-common
+sudo apt-add-repository -y ppa:fish-shell/release-3
+
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
-&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
-&& sudo apt update
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+
+sudo apt update
 DEBIAN_FRONTEND=noninteractive sudo apt-get -y install --no-install-recommends fish gh
+
 sudo chsh -s /usr/bin/fish $USER
 fish ./setup.fish
 
-# http://asdf-vm.com/guide/getting-started.html#_3-install-asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
-mkdir -p ~/.config/fish/completions; ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
-echo "source ~/.asdf/asdf.fish" >> $HOME/.config/fish/config.fish
+# https://mise.jdx.dev/getting-started.html
+curl https://mise.jdx.dev/install.sh | sh
+echo '~/.local/bin/mise activate fish | source' >> ~/.config/fish/config.fish
 
-source ~/.asdf/asdf.sh
-asdf plugin add nodejs
-asdf plugin add golang
-asdf install golang latest; asdf global golang latest
-asdf plugin add rust
-# asdf install rust latest && asdf global rust latest
-asdf plugin add deno
-# asdf install deno latest && asdf global deno latest
-
-go install github.com/jesseduffield/lazygit@latest
-asdf reshim
+mise u -g golang@latest
+mise x -- go install github.com/jesseduffield/lazygit@latest
+mise reshim
