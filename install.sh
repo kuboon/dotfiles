654a2cdf05338abbfc166e44b8bcf765ae08af98
Ohkubo KOHEI <kuboon@trick-with.net>
2024-01-15 15:35:47 +0900

    remove fish ppa

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -ue
 
-sudo apt update
-sudo apt install -y software-properties-common
-sudo apt-add-repository -y ppa:fish-shell/release-3
-
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
