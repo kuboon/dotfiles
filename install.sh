adce2036314f84ca23a8ebaf586c5d56dfce94d2
Ohkubo KOHEI <o@kbn.one>
2026-02-03 03:23:06 +0000

    add mise rg, gist public key

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ echo 'eval "$('"$install_path"' activate --shims bash)"' >> ~/.bashrc
 echo 'eval "$('"$install_path"' activate --shims fish)"' >> ~/.config/fish/config.fish
 
 mise settings add idiomatic_version_file_enable_tools ruby
-mise u -gy lazygit gh
+mise u -gy lazygit gh rg
 
 mkdir -p ~/.config/lazygit
 ln -sf ~/dotfiles/lazygit/config.yml ~/.config/lazygit/config.yml
@@ -34,6 +34,7 @@ ln -sf ~/dotfiles/skills ~/.claude/skills
 
 mkdir -p ~/.ssh
 echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" >> ~/.ssh/known_hosts
+echo "gist.github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" >> ~/.ssh/known_hosts
 
 git config --global user.name "Ohkubo KOHEI";
 git config --global gpg.format ssh
